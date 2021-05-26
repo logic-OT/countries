d51f508b0a08031b65eea96f46772a26dbe764e3
logic-OT <61668807+logic-OT@users.noreply.github.com>
2021-05-26 13:02:33 +0000

    Update asgi.py

--- countries/asgi.py
+++ countries/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+django_asgi = get_asgi_application() 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'countries.settings')
 
@@ -20,6 +21,6 @@ from channels.auth import AuthMiddlewareStack
 from country_info.routing import ws_urls
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi,
     'websocket': AuthMiddlewareStack(URLRouter(ws_urls))
 })
