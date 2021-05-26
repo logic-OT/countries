7c3280c80571f1c0419a5faa1cd24573c6dc77f3
logic-OT <61668807+logic-OT@users.noreply.github.com>
2021-05-26 13:06:50 +0000

    Update asgi.py

--- countries/asgi.py
+++ countries/asgi.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-django_asgi = get_asgi_application() 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'countries.settings')
 
+django_asgi = get_asgi_application() 
 
 
 from channels.routing import ProtocolTypeRouter, URLRouter
